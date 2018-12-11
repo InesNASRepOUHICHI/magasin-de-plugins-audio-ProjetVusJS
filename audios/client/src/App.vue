ba2bc9b364efedae4c97ad2ba5ab118f5ec5212d
Ines NASR <ines.nasr.93@gmail.com>
2018-12-11 23:43:31 +0100

    Fix css conflicts

--- audios/client/src/App.vue
+++ audios/client/src/App.vue
@@ -1,21 +1,18 @@
 <template>
  <b-container id="app">
-    <b-row>
-      <b-col>
-        <!--navbar/-->
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col>
         <router-view/>
-      </b-col>
-    </b-row>
   </b-container>
 </template>
 <script>
 
+import AppAudio from './components/AppAudio'
+import AddAudio from './components/AddAudio'
 
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    AppAudio,
+    AddAudio
+  }
 }
 </script>
